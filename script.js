b4a58c86063ed46eb3ebe5a9cf097bbb930ff84a
BernaBirki <bernabirki@hotmail.ch>
2015-06-05 11:35:07 +0200

    grade die vierte

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
 		navigator.geolocation.getCurrentPosition(function(position) {
 			console.log(position);
 
-			$('.position').text(position.coords.position);
 			$('.longitude').text(position.coords.longitude);
 			$('.latitude').text(position.coords.latitude);
 			$('.accuracy').text(position.coords.accuracy);
