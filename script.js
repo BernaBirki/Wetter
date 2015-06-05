42c6ad492053a50f479c20a91c3a6075c9bc3c79
BernaBirki <bernabirki@hotmail.ch>
2015-06-05 11:19:01 +0200

    laengengrad...

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
 		navigator.geolocation.getCurrentPosition(function(position) {
-
 			console.log(position);
+			$('.longitude').text(position.coords.longitude);
+			$('.latitude').text(position.coords.latitude);
+			$('accurancy').text(position.coords.accurancy);
+
 	});
 
 });
