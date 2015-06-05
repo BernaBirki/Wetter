a89c7aa920b673378c06524fe0fd90ff672d9d14
BernaBirki <bernabirki@hotmail.ch>
2015-06-05 11:24:00 +0200

    grade neu

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
 		navigator.geolocation.getCurrentPosition(function(position) {
 			console.log(position);
+
 			$('.longitude').text(position.coords.longitude);
 			$('.latitude').text(position.coords.latitude);
-			$('accurancy').text(position.coords.accurancy);
+			$('accuracy').text(position.coords.accuracy);
 
 	});
 
