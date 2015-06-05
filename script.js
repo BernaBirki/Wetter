2e1f17dad16bde06c51440e0bb737f000532e980
BernaBirki <bernabirki@hotmail.ch>
2015-06-05 11:28:01 +0200

    grade die zweite

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ $(document).ready(function() {
 		navigator.geolocation.getCurrentPosition(function(position) {
 			console.log(position);
 
+			$('.position').text(position.coords.position);
 			$('.longitude').text(position.coords.longitude);
 			$('.latitude').text(position.coords.latitude);
-			$('accuracy').text(position.coords.accuracy);
+			$('.accuracy').text(position.coords.accuracy);
 
 	});
 
