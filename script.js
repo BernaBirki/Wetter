cd733c9a631c5895cc7851cf8df067484cc260b5
BernaBirki <bernabirki@hotmail.ch>
2015-06-12 09:03:43 +0200

    Fr. 12.6.

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 $(document).ready(function() {
 		navigator.geolocation.getCurrentPosition(function(position) {
-			console.log(position);
+			//console.log(position);
 
-			$('.longitude').text(position.coords.longitude);
-			$('.latitude').text(position.coords.latitude);
-			$('.accuracy').text(position.coords.accuracy);
+			var koordinaten= {
+				longitude: position.coords.longitude,
+				latitude: position.coords.latitude
+			};
+
+			// $('.longitude').text(position.coords.longitude);
+			// $('.latitude').text(position.coords.latitude);
+			// $('.accuracy').text(position.coords.accuracy);
+
+			$.ajax({
+				url: 'https://api.forecast.io/forecast/f044c2cc88a6bb408f9d3092d6a19871/37.8267,-122.423' + koordinaten.latitude ',' + koordinaten.longitude
+				data: {
+
+					units:'si',
+					lang: 'de'
+				},
+				dataType: 'jsonp'
+			}).done function(data) {
+				console.log(data);
+			}
 
 	});
 
