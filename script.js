ecb6a3d7aa6e7c6a0e97ec92776abcb085b8f008
BernaBirki <bernabirki@hotmail.ch>
2015-06-12 09:27:31 +0200

    vierte

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 $(document).ready(function() {
-		navigator.geolocation.getCurrentPosition(function(position) {
-			//console.log(position);
+	navigator.geolocation.getCurrentPosition(function(position) {
+		//console.log(position);
 
-			var koordinaten= {
-				longitude: position.coords.longitude,
-				latitude: position.coords.latitude
-			};
+		var koordinaten= {
+			longitude: position.coords.longitude,
+			latitude: position.coords.latitude
+		};
 
-			// $('.longitude').text(position.coords.longitude);
-			// $('.latitude').text(position.coords.latitude);
-			// $('.accuracy').text(position.coords.accuracy);
+		// $('.longitude').text(position.coords.longitude);
+		// $('.latitude').text(position.coords.latitude);
+		// $('.accuracy').text(position.coords.accuracy);
 
-			$.ajax({
-				url: 'https://api.forecast.io/forecast/f044c2cc88a6bb408f9d3092d6a19871/37.8267,-122.423' + koordinaten.latitude ',' + koordinaten.longitude,
-				data: {
+		$.ajax({
+			url: 'https://api.forecast.io/forecast/f044c2cc88a6bb408f9d3092d6a19871/' + koordinaten.latitude ',' + koordinaten.longitude,
+			data: {
 
-					units:'si',
-					lang: 'de'
-				},
-				dataType: 'jsonp'
-			}).done (function(data) {
-				console.log(data);
+				units:'si',
+				lang: 'de'
+			},
+			dataType: 'jsonp'
+		}).done (function(data) {
+			console.log(data);
 
-			});
+		});
 	});
 
 });
