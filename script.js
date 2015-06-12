0bcbb524c36336e275c2bc71043567807b00ba19
BernaBirki <bernabirki@hotmail.ch>
2015-06-12 10:58:04 +0200

    temp und koords

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
 			latitude: position.coords.latitude
 		};
 
+
 		// $('.longitude').text(position.coords.longitude);
 		// $('.latitude').text(position.coords.latitude);
 		// $('.accuracy').text(position.coords.accuracy);
@@ -21,7 +22,19 @@ $(document).ready(function() {
 			dataType: 'jsonp'
 		}).done (function(data) {
 			console.log(data);
+			$('.temp').text(data.currently.apparentTemperature+ '°C');
+
+			//google geocoding
+			$.ajax({
+				url:'https://maps.googleapis.com/maps/api/geocode/json',
+				data: {
+					latlng: koodinaten.latitude + ',' + koordinaten.longitude,
+					key: 'AIzaSyDgYh-UffzCV54XCcReML4WSqyb0_zv8x8',
+					language: 'de'
+				}
+			}).done (function(data){
+				console.log(data);
+			});
 		});
 	});
-
 });
